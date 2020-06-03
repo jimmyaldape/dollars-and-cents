7230e9f62b22bb570371a558d4a5e69b60ff3acb
Jimmy <jimmy.aldape@medsch.ucr.edu>
2020-06-02 19:09:28 -0700

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Header } from './components/Header'
-import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="w-1/3">
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <h2 className="my-4">Dollars and Cents</h2>
+        <h2 className="my-4 text-2xl text-center">Dollars and Cents</h2>
     )
 }
