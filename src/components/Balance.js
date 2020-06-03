38fab5c821ba56d1868f6c899975fc44810e89cd
Jimmy <jimmy.aldape@medsch.ucr.edu>
2020-06-02 23:29:12 -0700

    added values from global state

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <>
            <h4 className="text-sm uppercase">Your Balance</h4> 
-           <h1 className="text-2xl">$0.00</h1>
+           <h1 className="text-3xl">${total}</h1>
         </>
     )
 }
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const income = amounts.filter( item => item > 0)
+                            .reduce((acc, item) => (acc += item), 0)
+                            .toFixed(2);
+    const expense = (amounts.filter( item => item < 0)
+                            .reduce((acc, item) => (acc += item), 0) * -1)
+                            .toFixed(2);
+
     return (
         <div className="flex flex-row border border-gray-500 border-black mt-4 bg-white rounded-lg shadow text-center">
             <div className="w-1/2 p-4 border-r border-gray-300">
                 <h4 className="text-xs uppercase">Income</h4>
-                <p className="text-lg text-green-400">$0.00</p>
+                <p className="text-lg text-green-400">${income}</p>
             </div>
             <div className="w-1/2 p-4">
                 <h4 className="text-xs uppercase">Expense</h4>
-                <p className="text-lg text-red-400">$0.00</p>
+                <p className="text-lg text-red-400">${expense}</p>
             </div>
         </div>
     )
